9fde4dfe56ab4a01d982e1c02d32539a00f01f36
Emerson Oliveira <emerson.ipynb@gmail.com>
2025-11-02 08:29:04 -0300

    novas colunas e resultados de metodos no arquivo 'saidas'

--- dia04/01_novas_colunas.py
+++ dia04/01_novas_colunas.py
@@ -1,10 +1,23 @@
 # %%
 import pandas as pd
-
+import numpy as np
 # %%
 df = pd.read_csv('../data/clientes.csv', sep=';')
 cresc = df.sort_values('DtCriacao')
-# df.head(15)
+df.head(15)
 
 # %%
-df['qtdePontos'] + 100
+# cria uma nova coluna, adicionando +100 pra cada linha da qtdePontos
+df['pontos_100'] = df['qtdePontos'] + 100
+
+# %%
+# cria uma nova coluna com a soma de 
+df['emailTwitch'] = df['flEmail'] + df['flTwitch']
+df.sample(5)
+
+# %%
+df['emailTwitch_and'] = df['flEmail'] * df['flTwitch']
+df.sample(5)
+
+# %%
+df['qtdePontos'].describe()
